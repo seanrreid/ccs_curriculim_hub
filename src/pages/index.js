8bc32c49a29d090d3eac07815fc898e66e896025
Sean R Reid <sean@torchcodelab.com>
2025-02-12 15:02:14 -0500

    Removing default data from initial install

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
     return (
         <Layout
             title={`Hello from ${siteConfig.title}`}
-            description='Description will go into a meta tag in <head />'
+            description=''
         >
             <HomepageHeader />
             <main class={clsx('container', styles.main)}>
